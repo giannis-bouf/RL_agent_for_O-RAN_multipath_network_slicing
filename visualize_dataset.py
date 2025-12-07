d69309e2f132070f7bce518e410aafb5d1185a29
giannis-bouf <133651696+giannis-bouf@users.noreply.github.com>
2025-12-07 17:18:24 +0200

    Update dataset visualization with configurations
    Added visualization for average number of configurations.

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -4,13 +4,14 @@ import numpy as np
 import pickle
 from arrival import SliceReqGenerator
 
-with open("dataset/historic_data.pkl", "rb") as f:
+with open("datasets/dataset2/historic_data.pkl", "rb") as f:
     dataset = pickle.load(f)
 
 arrivals = np.zeros(12)
 arr_time = []
 act_time = []
 active = np.zeros(12)
+configs = np.zeros(12)
 
 for inst in dataset:
     for sl in inst.arriving_reqs:
@@ -19,6 +20,7 @@ for inst in dataset:
         for t in range(sl[1], sl[1] + sl[2]):
             act_time.append(t+1)
     active += inst.active
+    configs += inst.configurations
 
 plt.figure(figsize=(6, 4))
 sns.histplot(x=arr_time, label='Dataset', kde=False, stat='count', bins=30, weights=np.ones_like(arr_time) / 1000, color='blue')
@@ -46,6 +48,17 @@ plt.yticks(fontweight='bold')
 plt.legend(fontsize='medium', frameon=True).set_title(None)  # Remove legend title
 for text in plt.gca().get_legend().get_texts():
     text.set_fontweight('bold')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(6, 4))
+plt.bar(np.arange(1, 13), configs / 1000)
+
+plt.xlabel('Time Slot', fontweight='bold')
+plt.xticks(np.arange(1, 13, 1), fontweight='bold')
+plt.ylabel('Average Number of Configurations', fontweight='bold')
+plt.yticks(fontweight='bold')
 plt.grid(True)
 plt.tight_layout()
 plt.show()
