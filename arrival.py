b88c7b9faaf2a95129990dd0bb4b0b954a31d29e
giannis-bouf <133651696+giannis-bouf@users.noreply.github.com>
2026-01-14 17:30:15 +0200

    Refactor SliceReqGenerator and update file paths
    Updated the slice request generator to simplify computational demand handling and adjusted file paths for saving data.

--- arrival.py
+++ arrival.py
@@ -12,7 +12,6 @@ class SliceReqGenerator:
         self.C_max = C_max
         self.arriving_reqs = []
         self.active = np.zeros(12)
-        self.configurations = np.zeros(12)
         total = np.random.randint(20, 30)
         self.generate_reqs(S=total, H=12, t_0=0)
 
@@ -38,73 +37,36 @@ class SliceReqGenerator:
 
             # # Arriving time of slice request
             # Normal Distribution
-            #mu = (H+t_0)/2-1
-            #sigma = 0.9
-            #t_s = int(np.clip(np.random.normal(loc=mu, scale=sigma), t_0, H + t_0-1))
+            # mu = (H+t_0)/2-1
+            # sigma = 0.9
+            # t_s = int(np.clip(np.random.normal(loc=mu, scale=sigma), t_0, H + t_0-1))
 
             # Poisson distribution
             # t_s = np.clip(np.random.poisson(lam=mu/2), t_0, H + t_0)
 
             # Exponential distribution
-            t_s = int(np.clip(np.random.exponential(scale=(H+t_0)/4), t_0, H + t_0-1))
+            # t_s = int(np.clip(np.random.exponential(scale=(H+t_0)/4), t_0, H + t_0-1))
 
             # Beta Distribution
-            #a, b = 10, 2
-            #t_s = int(np.clip(np.random.beta(a, b) * (H + t_0 - 1), t_0, H + t_0 - 1))
+            # a, b = 10, 2
+            # t_s = int(np.clip(np.random.beta(a, b) * (H + t_0 - 1), t_0, H + t_0 - 1))
 
             # Uniform Distribution
-            #t_s = np.random.randint(t_0, H + t_0)
+            t_s = np.random.randint(t_0, H + t_0)
 
             ht_s = min(np.random.randint(H / 4, H / 2), t_0 + H - t_s) # Slice's holding time
             for t in range(t_s, t_s + ht_s):
                 self.active[t] = self.active[t] + 1
-            self.configurations[t_s] += 1
 
             F_s = np.random.randint(3,self.Fs_max + 1) # SFC's size
-            C_s = [] # Computational demand for each VNF per time step
-            T_rec_s = set() # Time steps where slice will need reconfiguration
-            for k in range(F_s):
-                if k == 0:
-                    # Computational demand for VNF 0 is always 1
-                    C_s.append([1]*ht_s)
-                else:
-                    # Random choice of reconfiguration count
-                    # This way we can control how many reconfiguration requests will occur
-                    changes = np.random.choice([0, 1, 2], p=[0.3, 0.5, 0.2])
-
-                    # If n changes doesn't fit in slice's holding time, manage accordingly
-                    max_changes = max(0, ht_s - 1)
-                    changes = min(changes, max_changes)
-
-                    if changes == 0:
-                        C_s.append([np.random.choice(range(1, C_max+1), p=[0.25,0.75])]*ht_s)
-                        continue
-
-                    # Choose changing points randomly
-                    T_rec_k = sorted(np.random.choice(range(1, ht_s), size=changes, replace=False))
-                    segments = changes + 1
-                    # Choose the value of each segment
-                    demands = [np.random.choice(range(1, C_max+1), p=[0.5,0.5]) for _ in range(segments)]
-
-                    C_k = []
-                    prev_reconfig = 0
-                    for i, reconfig in enumerate(T_rec_k + [ht_s]):
-                        C_k.extend([demands[i]] * (reconfig - prev_reconfig))
-                        prev_reconfig = reconfig
-                    
-                    C_s.append(C_k)
-                
-                for t in range(1, ht_s):
-                    if C_s[k][t] != C_s[k][t-1]:
-                        T_rec_s.add(t)
-                
-                for t in T_rec_s:
-                    self.configurations[t_s + t] += 1
+            C_s = [1] # Computational demand for each VNF (demand for VNF 0 is always 1)
+            for _ in range(1, F_s):
+                C_s.append(np.random.choice(range(1, C_max+1), p=[0.25,0.75]))
 
             self.arriving_reqs.append([F_s, t_s, ht_s, delta_s, C_s])
 
 def load():
-    with open('datasets/dataset2/historic_data.pkl', 'rb') as f:
+    with open('datasets/datasetX/historic_data.pkl', 'rb') as f:
         loaded_episodes = pickle.load(f)
         print(f"Loaded {len(loaded_episodes)} episodes.")
         print(loaded_episodes[0].arriving_reqs)  # Print the first episode for verification
@@ -123,15 +85,14 @@ if __name__ == '__main__':
         all_episodes.append(episode)
 
     # Save episodes to pickle file
-    with open('datasets/dataset2/historic_data.pkl', 'wb') as f:
+    with open('datasets/datasetX/historic_data.pkl', 'wb') as f:
         pickle.dump(all_episodes, f)
 
     for _ in range(tests):
         episode = SliceReqGenerator(Fs_max, C_max)
         test_episodes.append(episode)
 
-    with open('datasets/dataset2/test_data.pkl', 'wb') as f:
+    with open('datasets/datasetX/test_data.pkl', 'wb') as f:
        pickle.dump(test_episodes, f)
 
     load()
-
